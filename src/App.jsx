aeed7f6a3593fc2c2b539cbdc85c1a5b112c6923
KornepalliTejasree <tejasree7718@gmail.com>
2026-06-25 19:43:11 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import {createHashRouter,RouterProvider} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Favorites from './pages/Favorites'
 import MovieDetails from './pages/MovieDetails'
 import Home from './pages/Home'
 import Movie from './components/Movie'
 const App = () => {
-  const router=createBrowserRouter([
+  const router=createHashRouter([
     {path:"/",
       element:<div>
         <Navbar />
